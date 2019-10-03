fc3f977ee220fe4f163114e2da56869829d83ad3
janetpeng97 <55019632+janetpeng97@users.noreply.github.com>
2019-10-03 11:59:07 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,22 +1,16 @@
-/research/dept7/wlchen/miniconda3/envs/pdenv/bin/python3 train.py \
+python3 test.py \
 --gpu_ids 0 \
---netG custom \
---netD n_layers \
---pool_size 0 \
---batch_size 8 \
+--display_winsize 256 \
 --preprocess resize_and_crop \
---dataset_mode aligned \
---load_size 256 \
---crop_size 256 \
---niter 40 \
---niter_decay 30 \
---print_freq 500 \
---save_epoch_freq 20 \
---output_nc 1 \
---init_type kaiming \
---norm batch \
---dataroot /research/dept7/wlchen/dupeng/Binary \
---name Custom_pix2pix_binary_60epoch_256 \
+--load_size 512 \
+--crop_size 512 \
+--dataroot /cephfs/person/asdfghchen/LithoGAN/data/LithoGAN/Binary \
+--name LithoGAN_pix2pix_binary_200epoch_512 \
 --model pix2pix \
+--netG unet_256 \
+--output_nc 1 \
 --direction AtoB \
---display_id 0
+--dataset_mode aligned \
+--eval \
+--num_test 2170 \
+--norm batch
