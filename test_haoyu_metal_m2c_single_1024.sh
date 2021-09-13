1db467564e6e47b1d5c25e3170e1c0a767043f07
Guojin Chen <cgjhaha@qq.com>
2021-09-13 09:13:12 +0800

    ✨ feat: update test metal

---
+++ test_haoyu_metal_m2c_single_1024.sh
@@ -0,0 +1,21 @@
+/research/d4/gds/gjchen21/miniconda3/envs/torch1.2/bin/python test.py \
+--gpu_ids 0 \
+--checkpoints_dir /research/d4/gds/gjchen21/github/dls-dmo/checkpoints \
+--dataroot /research/d4/gds/gjchen21/datasets/datasets/dlsopc_datasets/haoyu_data/metal_dls/ \
+--netG new_dcupp \
+--netD naive6_nl \
+--display_winsize 1024 \
+--preprocess resize_and_crop \
+--dataset_mode aligned \
+--load_size 1024 \
+--crop_size 1024 \
+--results_dir /research/d4/gds/gjchen21/github/dls-dmo/results \
+--name dlsdmo_haoyu_metal_m2c_single_1024 \
+--model pix2pix \
+--input_nc 1 \
+--output_nc 1 \
+--direction AtoB \
+--eval \
+--num_test 200000 \
+--norm instance \
+--epoch 70
